ca459a808a0a66a5daf8047ede971e82aaa2c160
Sanjaykumar0072 <b.sanjaykumar2000@gmail.com>
2023-03-08 12:17:26 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    debug: true,
+    port: 4000,
+    useNewUrlParser: true,
+    mongodb: "mongodb://localhost:27017/progress",
+    // analytics: 'UA-151898306-1',
+    version: '1.4.0',
+};
+module.exports = config;
